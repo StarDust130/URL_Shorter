09ad445733f9352a3fcf5caaa3ec6e40955a08aa
ChandraShekhar <csyadav0513@gmail.com>
2024-04-09 13:20:59 +0530

    Update uuid package to version 9.0.1

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,10 @@
 const User = require("../models/user");
+const {} = require("uuid");
 
 const handleUserSignup = async (req, res) => {
   const { name, email, password } = req.body;
   const user = await User.create({ name, email, password });
-  return  res.redirect("/");
+  return res.redirect("/");
 };
 
 const handleUserLogin = async (req, res) => {
